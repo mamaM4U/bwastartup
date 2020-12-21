2f61a8c54b91b8c28c4578c6750eef087733204c
Afdrian Juarlin <afdrian23>
2020-12-22 00:25:36 +0700

    auth middleware

--- handler/user.go
+++ handler/user.go
@@ -135,8 +135,8 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		return
 	}
 
-	//jwt soon
-	userID := 1
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 	err = c.SaveUploadedFile(file, path)
 	if err != nil {
--- user/service.go
+++ user/service.go
@@ -11,6 +11,7 @@ type Service interface {
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
 	SaveAvatar(id int, fileLocation string) (User, error)
+	GetUserById(id int) (User, error)
 }
 
 type service struct {
@@ -97,4 +98,16 @@ func (s *service) SaveAvatar(id int, fileLocation string) (User, error) {
 
 	return updatedUser, nil
 
+}
+
+func (s *service) GetUserById(id int) (User, error) {
+	user, err := s.repository.FindById(id)
+	if err != nil {
+		return user, err
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("No user found on that id")
+	}
+	return user, nil
 }
