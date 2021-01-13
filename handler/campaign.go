275cc462ac904f4c84afa4fd1f8226e624cd4c2d
Afdrian Juarlin <afdrian23>
2021-01-13 21:37:16 +0700

    get campaign detail endpoint

--- handler/campaign.go
+++ handler/campaign.go
@@ -9,17 +9,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type campaignHanlder struct {
+type campaignHandler struct {
 	service campaign.Service
 }
 
-func NewCampaignHandler(service campaign.Service) *campaignHanlder {
-	return &campaignHanlder{service}
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
 }
 
 //api/v1/campaigns
 
-func (h *campaignHanlder) GetCampaigns(c *gin.Context) {
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	userID, _ := strconv.Atoi(c.Query("user_id"))
 
 	campaigns, err := h.service.GetCampaigns(userID)
@@ -31,4 +31,30 @@ func (h *campaignHanlder) GetCampaigns(c *gin.Context) {
 	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 
+}
+
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	// handler : mapping id  yg di url ke struct input di service (call formatter)
+	// service : inputnya struct input => menangkap id
+	// repository : get campaign by id
+
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaignDetail, err := h.service.GetCampaignByID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success get campaign Detail", http.StatusOK, "success", campaign.FormatCampaignDetail(campaignDetail))
+	c.JSON(http.StatusOK, response)
 }
