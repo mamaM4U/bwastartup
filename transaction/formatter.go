7436e188a9972d26b1746c91c7c524839014bc5c
Afdrian Juarlin <afdrian23>
2021-01-18 19:22:08 +0700

    transaction endpoiont

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -1,1 +1,35 @@
 package transaction
+
+import "time"
+
+type CampaignTransactionFormatter struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Amount    int       `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func FormatCampaignTransaction(transaction Transaction) CampaignTransactionFormatter {
+	formatter := CampaignTransactionFormatter{}
+	formatter.ID = transaction.ID
+	formatter.Name = transaction.User.Name
+	formatter.Amount = transaction.Amount
+	formatter.CreatedAt = transaction.CreatedAt
+
+	return formatter
+}
+
+func FormatCampaignTransactions(transactions []Transaction) []CampaignTransactionFormatter {
+	transactionFormatter := []CampaignTransactionFormatter{}
+
+	if len(transactions) == 0 {
+		return transactionFormatter
+	}
+
+	for _, transaction := range transactions {
+		formatter := FormatCampaignTransaction(transaction)
+		transactionFormatter = append(transactionFormatter, formatter)
+	}
+
+	return transactionFormatter
+}
